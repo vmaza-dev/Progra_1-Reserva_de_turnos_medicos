b25f2085aae1654855b1184bdb2b3864272285f7
simavila <simavila@uade.edu.ar>
2025-08-23 17:22:26 -0300

    Se ampliaron las funciones basicas y se mejoro la salida.

--- pacientes.py
+++ pacientes.py
@@ -8,25 +8,42 @@
 import random, fun_aux
 print("l")
 
+def crear_paciente(id):
+
+    dni= int(input("Ingrese su dni: "))
+    nombreCompleto= int(input("Ingrese su nombre completo: "))
+    edad= int(input("Ingrese su edad: "))
+    obra_social= (input("Ingrese su obra social: "))
+    estado= 1
+    paciente=[id,dni,nombreCompleto,edad,obra_social,estado]
+
+    return paciente
+
+
 def crear_pacientes_random(pacientes, cantCrear):
     for i in range(cantCrear):
-        nombre = random.choice(fun_aux.nombres) + " " + random.choice(fun_aux.apellidos)
-        pacientes.append([random.randint(1000, 9999), random.randint(10000000, 99999999),
-                           nombre, random.randint(3, 100)])
+        nombreCompleto = random.choice(fun_aux.nombres) + " " + random.choice(fun_aux.apellidos)
+        dni= random.randint(10000000, 99999999)
+        edad= random.randint(3,99)
+        obra_social= random.choice(["OSDE", "Swiss Medical", "VICMAZA", "Galeno","Particular"])
+        estado=1
+        id=random.randint(1000,9999)
+        pacientes.append([id,dni,nombreCompleto,edad,obra_social,estado])
+
         
 def leer_pacientes(pacientes):
+    ancho=50
     for pac in pacientes:
-        print("---------------------------------------")
-        print(f"PACIENTE: {pac[2]} | ID: {pac[0]}")
-        print(f"DNI: {pac[1]}\nEDAD: {pac[3]}")
+        print("=" * ancho)
+        print(f" PACIENTE: {pac[2]} ".center(ancho, "="))
+        print(f"ID: {str(pac[0]).ljust(10)} | DNI: {str(pac[1]).ljust(12)}")
+        print(f"EDAD: {str(pac[3]).ljust(8)} | OBRA SOCIAL: {pac[4]}")
+        print(f"ESTADO: {'Activo' if pac[5]==1 else 'Inactivo'}".center(ancho))
+        print("=" * ancho)
+        print()  # línea en blanco entre pacientes
 
-#[ID, DNI, NOMBRE, EDAD]
-#pacientes = [
-#    [1, "11223344", "Jose Diaz"],
-#    [2, "12131415", "Andres Jimenez"],
-#    [3, "33366633", "Micaela Macri"],
-#    [4, "77754894", "Fernanda Fernandez"]
-#]
+#[ID, DNI, NOMBRE, EDAD], OBRA SOCIAL,ESTADO]
+
 
 pacientes = []
 crear_pacientes_random(pacientes, 10)
