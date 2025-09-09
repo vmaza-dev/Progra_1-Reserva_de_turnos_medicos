6b656dbbf27fba2c5593227e7b75ff103936101b
Victor Alejandro Maza <105168404+vmaza-dev@users.noreply.github.com>
2025-09-08 22:30:49 -0300

    Delete turnos directory
    Despaquetizadp turnos

--- turnos/__init__.py
+++
@@ -1,8 +0,0 @@
-# ==============================================================================
-# Autor: Victor Maza
-# Descripción: __init__.py turnos
-# Fecha de creación: 30/08/2025
-# ==============================================================================
-
-from .cre_leer import main_crear_leer, logo_turnos
-
